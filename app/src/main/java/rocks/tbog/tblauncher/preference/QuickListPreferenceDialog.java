1509abc0f04cb4a2be0dae845c8d566d2815c8c4
TBog <thetbog@gmail.com>
2020-09-09 13:07:27 +0300

    fix edit QuickList content layout in settings

--- app/src/main/java/rocks/tbog/tblauncher/preference/QuickListPreferenceDialog.java
+++ app/src/main/java/rocks/tbog/tblauncher/preference/QuickListPreferenceDialog.java
@@ -2,6 +2,8 @@ package rocks.tbog.tblauncher.preference;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
 import androidx.preference.PreferenceDialogFragmentCompat;
 
@@ -10,6 +12,7 @@ import rocks.tbog.tblauncher.EditQuickList;
 public class QuickListPreferenceDialog extends PreferenceDialogFragmentCompat {
 
     private final EditQuickList mEditor = new EditQuickList();
+    private View mDialogView = null;
 
     public static QuickListPreferenceDialog newInstance(String key) {
         QuickListPreferenceDialog fragment = new QuickListPreferenceDialog();
@@ -31,9 +34,27 @@ public class QuickListPreferenceDialog extends PreferenceDialogFragmentCompat {
     protected void onBindDialogView(View view) {
         super.onBindDialogView(view);
 
+        mDialogView = view;
         mEditor.bindView(view);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
 
-
+        // hack to have the LinearLayout weight work
+        ViewParent parent = mDialogView != null ? mDialogView.getParent() : null;
+        while (parent instanceof ViewGroup) {
+            ViewGroup layout = (ViewGroup) parent;
+            ViewGroup.LayoutParams params = layout.getLayoutParams();
+            if (params.height != ViewGroup.LayoutParams.MATCH_PARENT) {
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                params.height = ViewGroup.LayoutParams.MATCH_PARENT;
+                layout.setLayoutParams(params);
+            }
+            if (layout.getId() == android.R.id.content)
+                break;
+            parent = parent.getParent();
+        }
+    }
 }
