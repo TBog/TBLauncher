3c6c2f89d014ee561bf5eba6859b85bffc9fb239
TBog <thetbog@gmail.com>
2020-11-17 20:38:16 +0200

    add frequency, frecency and adaptive

--- app/src/main/java/rocks/tbog/tblauncher/PrefCache.java
+++ app/src/main/java/rocks/tbog/tblauncher/PrefCache.java
@@ -8,12 +8,14 @@ import androidx.preference.PreferenceManager;
 public class PrefCache {
 
     private static int RESULT_HISTORY_SIZE = 0;
+    private static int RESULT_HISTORY_ADAPTIVE = 0;
 
     private PrefCache() {
     }
 
     public static void resetCache() {
         RESULT_HISTORY_SIZE = 0;
+        RESULT_HISTORY_ADAPTIVE = 0;
     }
 
     public static int getResultHistorySize(Context context) {
@@ -24,5 +26,15 @@ public class PrefCache {
             RESULT_HISTORY_SIZE = pref.getInt("result-history-size", defaultSize);
         }
         return RESULT_HISTORY_SIZE;
+    }
+
+    public static int getHistoryAdaptive(Context context) {
+        if (RESULT_HISTORY_ADAPTIVE == 0)
+        {
+            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+            final int defaultSize = context.getResources().getInteger(R.integer.default_result_history_adaptive);
+            RESULT_HISTORY_ADAPTIVE = pref.getInt("result-history-adaptive", defaultSize);
+        }
+        return RESULT_HISTORY_ADAPTIVE;
     }
 }
