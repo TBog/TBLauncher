af33439a5d6f6ff8cd174365fa78c53088b44cde
TBog <thetbog@gmail.com>
2021-08-20 14:56:55 +0300

    remove AnimatedListView

---
+++ app/src/main/java/rocks/tbog/tblauncher/result/ReversibleAdapterRecyclerLayoutManager.java
@@ -0,0 +1,6 @@
+package rocks.tbog.tblauncher.result;
+
+public interface ReversibleAdapterRecyclerLayoutManager {
+    void setReverseAdapter(boolean reverseAdapter);
+    boolean isReverseAdapter();
+}
--- app/src/main/java/rocks/tbog/tblauncher/ui/RecyclerList.java
+++ app/src/main/java/rocks/tbog/tblauncher/ui/RecyclerList.java
@@ -1,17 +1,14 @@
 package rocks.tbog.tblauncher.ui;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.MotionEvent;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
-import rocks.tbog.tblauncher.result.CustomRecycleLayoutManager;
+import rocks.tbog.tblauncher.result.ReversibleAdapterRecyclerLayoutManager;
 
 public class RecyclerList extends RecyclerView {
     private static final String TAG = "list";
@@ -28,71 +25,18 @@ public class RecyclerList extends RecyclerView {
         super(context, attrs, defStyleAttr);
     }
 
-//    /**
-//     * Prevent this ListView from receiving any new touch events
-//     * <p>
-//     * Use {@link #unblockTouchEvents()} to end the blockage.
-//     */
-//    public void blockTouchEvents() {
-//        this.touchEventsBlocked = true;
-//        if (getLayoutManager() instanceof CustomRecycleLayoutManager) {
-//            CustomRecycleLayoutManager customRecycleLayoutManager = (CustomRecycleLayoutManager) getLayoutManager();
-//            customRecycleLayoutManager.setAutoScrollBottom(false);
-//        }
-//    }
-//
-//    /**
-//     * Stop preventing this ListView from receiving touch events
-//     */
-//    public void unblockTouchEvents() {
-//        this.touchEventsBlocked = false;
-//        if (getLayoutManager() instanceof CustomRecycleLayoutManager) {
-//            CustomRecycleLayoutManager customRecycleLayoutManager = (CustomRecycleLayoutManager) getLayoutManager();
-//            customRecycleLayoutManager.setAutoScrollBottom(true);
-//        }
-//    }
-//
-//    public boolean touchEventsBlocked() {
-//        return touchEventsBlocked;
-//    }
-
-    public void scrollToLastPosition() {
-        final int resultCount = getAdapter() != null ? getAdapter().getItemCount() : 0;
-        if (resultCount > 0) {
-            Log.d(TAG, "scrollToPosition( " + (resultCount - 1) + " )");
-            scrollToPosition(resultCount - 1);
+    public void scrollToFirstItem() {
+        final int adapterPos;
+        if (getLayoutManager() instanceof ReversibleAdapterRecyclerLayoutManager) {
+            ReversibleAdapterRecyclerLayoutManager lm = (ReversibleAdapterRecyclerLayoutManager) getLayoutManager();
+            adapterPos = lm.isReverseAdapter() ? (getLayoutManager().getItemCount() - 1) : 0;
+        } else {
+            adapterPos = 0;
         }
-    }
 
-    @SuppressLint("ClickableViewAccessibility")
-    @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-//        boolean handled = false;
-//        int action = ev.getActionMasked();
-//        if (!touchEventsBlocked && action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_MOVE)
-//            lastTouchPos.set(ev.getRawX(), ev.getRawY());
-//        MotionEvent e = MotionEvent.obtain(ev);
-//        e.offsetLocation(0f, 0f);
-//        if (touchEventsBlocked) {
-//            e.setAction(MotionEvent.ACTION_CANCEL);
-//            handled = super.onTouchEvent(ev);
-//        }
-//        e.recycle();
-//        return handled;
-        return super.onTouchEvent(ev);
-    }
-
-    public void setTranscriptMode(int mode) {
-        // no need, this was useful for the ListView
-    }
-
-    public void prepareChangeAnim() {
-        ItemAnimator itemAnimator = getItemAnimator();
-        if (itemAnimator != null)
-            itemAnimator.endAnimations();
-    }
-
-    public void animateChange() {
-        // no need, the RecyclerView takes care of it
+        if (adapterPos >= 0) {
+            Log.d(TAG, "scrollToPosition( " + adapterPos + " )");
+            scrollToPosition(adapterPos);
+        }
     }
 }
