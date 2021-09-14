3a03543a0f85758b7259f905a059f6f9dad6ab55
TBog <thetbog@gmail.com>
2021-09-14 14:41:21 +0300

    Add "remove from history" for shortcuts
    fix #204

--- app/src/main/java/rocks/tbog/tblauncher/entry/ShortcutEntry.java
+++ app/src/main/java/rocks/tbog/tblauncher/entry/ShortcutEntry.java
@@ -264,6 +264,7 @@ public final class ShortcutEntry extends EntryWithTags {
     @Override
     ListPopup buildPopupMenu(Context context, LinearAdapter adapter, View parentView, int flags) {
         adapter.add(new LinearAdapter.ItemTitle(context, R.string.popup_title_hist_fav));
+        adapter.add(new LinearAdapter.Item(context, R.string.menu_remove_history));
         adapter.add(new LinearAdapter.Item(context, R.string.menu_remove_shortcut));
         adapter.add(new LinearAdapter.Item(context, R.string.menu_favorites_add));
         adapter.add(new LinearAdapter.Item(context, R.string.menu_favorites_remove));
