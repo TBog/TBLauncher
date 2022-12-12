bc46274905f4a5c872bc6a4c2946b3a451f833f8
TBog <thetbog@gmail.com>
2022-12-12 23:23:30 +0200

    Add sort order to tag entry

--- app/src/main/java/rocks/tbog/tblauncher/searcher/ISearcher.java
+++ app/src/main/java/rocks/tbog/tblauncher/searcher/ISearcher.java
@@ -2,11 +2,13 @@ package rocks.tbog.tblauncher.searcher;
 
 import androidx.annotation.WorkerThread;
 
+import java.util.Collection;
+
 import rocks.tbog.tblauncher.entry.EntryItem;
 
 public interface ISearcher {
     @WorkerThread
-    boolean addResult(EntryItem... items);
+    boolean addResult(Collection<? extends EntryItem> items);
 
     boolean tagsEnabled();
 }
