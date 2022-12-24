727ee06b25734ca0966eb8e614031792aee955da
TBog <thetbog@gmail.com>
2022-12-24 18:16:27 +0200

    code restructure - recycler adapter base class

--- app/src/main/java/rocks/tbog/tblauncher/Behaviour.java
+++ app/src/main/java/rocks/tbog/tblauncher/Behaviour.java
@@ -1001,7 +1001,7 @@ public class Behaviour implements ISearchActivity {
             if (!state.isResultListVisible() && state.getDesktop() == LauncherState.Desktop.SEARCH)
                 showResultList(false);
         }
-        mResultAdapter.updateResults(results);
+        mResultAdapter.updateItems(results);
 
         if (!isRefresh) {
             // Make sure the first item is visible when we search
@@ -1016,7 +1016,7 @@ public class Behaviour implements ISearchActivity {
     @Override
     public void removeResult(@NonNull EntryItem result) {
         // Do not reset scroll, we want the remaining items to still be in view
-        mResultAdapter.removeResult(result);
+        mResultAdapter.removeItem(result);
     }
 
     @Override
--- app/src/main/java/rocks/tbog/tblauncher/result/RecycleAdapter.java
+++ app/src/main/java/rocks/tbog/tblauncher/result/RecycleAdapter.java
@@ -3,8 +3,6 @@ package rocks.tbog.tblauncher.result;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.drawable.Drawable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,58 +11,39 @@ import android.widget.Filter;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.preference.PreferenceManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
-import rocks.tbog.tblauncher.CustomizeUI;
 import rocks.tbog.tblauncher.TBApplication;
 import rocks.tbog.tblauncher.entry.EntryItem;
 import rocks.tbog.tblauncher.ui.ListPopup;
-import rocks.tbog.tblauncher.utils.UIColors;
 
-public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.Holder> {
+public class RecycleAdapter extends RecycleAdapterBase<RecycleAdapterBase.Holder> {
 
-    private static final String TAG = RecycleAdapter.class.getSimpleName();
-    /**
-     * Array list containing all the results currently displayed
-     */
-    @NonNull
-    private final ArrayList<EntryItem> results;
     @Nullable
     private ArrayList<EntryItem> resultsOriginal = null;
 
-    private int mDrawFlags;
-
-    private Filter mFilter = new RecycleAdapter.FilterById();
+    private final Filter mFilter = new RecycleAdapter.FilterById();
 
     public RecycleAdapter(@NonNull Context context, @NonNull ArrayList<EntryItem> results) {
-        this.results = results;
+        super(results);
         setHasStableIds(true);
         setGridLayout(context, false);
-    }
-
-    @Override
-    public int getItemViewType(int position) {
-        return ResultHelper.getItemViewType(results.get(position), mDrawFlags);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position < results.size() ? results.get(position).id.hashCode() : -1;
+        setOnClickListener(RecycleAdapter::onClick);
+        setOnLongClickListener(RecycleAdapter::onLongClick);
     }
 
     @NonNull
     @Override
-    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public RecycleAdapterBase.Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         final Context context = parent.getContext();
         final int layoutRes = ResultHelper.getItemViewLayout(viewType);
 
         LayoutInflater inflater = LayoutInflater.from(context);
         View itemView = inflater.inflate(layoutRes, parent, false);
 
-        return new Holder(itemView);
+        return new RecycleAdapterBase.Holder(itemView);
     }
 
     public void setGridLayout(@NonNull Context context, boolean bGridLayout) {
@@ -89,37 +68,9 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.Holder> 
         return drawFlags;
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull Holder holder, int position) {
-        final EntryItem result;
-        try {
-            result = results.get(position);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            Log.e(TAG, "pos=" + position + " size=" + results.size(), e);
-            return;
-        }
-
-        holder.setOnClickListener(view -> onClick(result, view));
-        holder.setOnLongClickListener(view -> onLongClick(result, view));
-
-        results.get(position).displayResult(holder.itemView, mDrawFlags);
-    }
-
-    @Override
-    public int getItemCount() {
-        return results.size();
-    }
-
-    public EntryItem getItem(int index) {
-        return results.get(index);
-    }
-
     public void onClick(int index, View anyView) {
-        final EntryItem result;
-        try {
-            result = results.get(index);
-        } catch (IndexOutOfBoundsException e) {
-            Log.e(TAG, "pos=" + index + " size=" + results.size(), e);
+        final EntryItem result = getItem(index);
+        if (result == null) {
             return;
         }
 
@@ -143,66 +94,28 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.Holder> 
         return false;
     }
 
+    @Override
     public void clear() {
         resultsOriginal = null;
-        final int itemCount = results.size();
-        results.clear();
-        notifyItemRangeRemoved(0, itemCount);
-    }
-
-    public void refresh() {
-        final int itemCount = results.size();
-        notifyItemRangeChanged(0, itemCount);
+        super.clear();
     }
 
     @SuppressLint("NotifyDataSetChanged")
-    public void updateResults(Collection<? extends EntryItem> results) {
+    public void updateItems(Collection<? extends EntryItem> results) {
         resultsOriginal = null;
-        this.results.clear();
-        this.results.addAll(results);
-        notifyDataSetChanged();
+        super.updateItems(results);
     }
 
-    public void removeResult(EntryItem result) {
-        int position = results.indexOf(result);
-        results.remove(result);
+    public void removeItem(EntryItem result) {
         if (resultsOriginal != null)
             resultsOriginal.remove(result);
-        notifyItemRemoved(position);
-    }
-
-    public void notifyItemChanged(EntryItem result) {
-        int position = results.indexOf(result);
-        Log.d(TAG, "notifyItemChanged #" + position + " id=" + result.id);
-        if (position >= 0)
-            notifyItemChanged(position);
+        super.removeItem(result);
     }
 
     public Filter getFilter() {
         if (resultsOriginal == null)
-            resultsOriginal = new ArrayList<>(results);
+            resultsOriginal = new ArrayList<>(entryList);
         return mFilter;
-    }
-
-    public static class Holder extends RecyclerView.ViewHolder {
-
-        public Holder(@NonNull View itemView) {
-            super(itemView);
-            itemView.setTag(this);
-
-            // we set background selector here to do it only once
-            int touchColor = UIColors.getResultListRipple(itemView.getContext());
-            Drawable selectorBackground = CustomizeUI.getSelectorDrawable(itemView, touchColor, false);
-            itemView.setBackground(selectorBackground);
-        }
-
-        public void setOnClickListener(@Nullable View.OnClickListener listener) {
-            itemView.setOnClickListener(listener);
-        }
-
-        public void setOnLongClickListener(@Nullable View.OnLongClickListener listener) {
-            itemView.setOnLongClickListener(listener);
-        }
     }
 
     private class FilterById extends Filter {
@@ -230,12 +143,12 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.Holder> 
         @Override
         protected void publishResults(CharSequence constraint, FilterResults filterResults) {
             if (filterResults != null) {
-                results.clear();
-                results.addAll((ArrayList<EntryItem>) filterResults.values);
+                entryList.clear();
+                entryList.addAll((ArrayList<EntryItem>) filterResults.values);
                 notifyDataSetChanged();
             } else if (resultsOriginal != null) {
-                results.clear();
-                results.addAll(resultsOriginal);
+                entryList.clear();
+                entryList.addAll(resultsOriginal);
                 resultsOriginal = null;
                 notifyDataSetChanged();
             }
